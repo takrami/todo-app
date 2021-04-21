019715d2b0b47c9fe9358acef1589eca3332e566
Atefeh H.Takrami <ati.takrami@gmail.com>
2021-04-21 17:36:38 +0200

    initilizing

---
+++ src/Header/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { HeaderContainer } from "./style";
+
+const Header = () => {
+  return <HeaderContainer>Header</HeaderContainer>;
+};
+
+export default Header;
---
+++ src/Header/style.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const HeaderContainer = styled.header`
+  background-color: red;
+`;
+
+export { HeaderContainer };
